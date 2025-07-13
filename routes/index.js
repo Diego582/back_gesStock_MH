e77ebdfd96190654ab12ee895515ef1b929677f5
Diego582 <penaomardiego@gmail.com>
2025-07-13 18:13:28 -0300

    se quito ruta upload

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ import userRouter from "./users.js";
 import authRouter from "./auth.js";
 import customerRouter from "./customer.js";
 import productsRouter from "./products.js";
-import uploadRouter from "./upload.js";
 import groupingsRouter from "./groupings.js";
 import productsBaseRouter from "./productsBase.js";
 import pricesRouter from "./prices.js"
@@ -16,7 +15,6 @@ router.use("/users", userRouter);
 router.use("/auth", authRouter);
 router.use("/customers", customerRouter);
 router.use("/products", productsRouter);
-router.use("/upload", uploadRouter);
 router.use("/groupings", groupingsRouter);
 router.use("/productsbase", productsBaseRouter);
 router.use("/prices", pricesRouter);
