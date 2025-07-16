6de64dd687bf3d3d275110092e4838682bd44125
Diego582 <penaomardiego@gmail.com>
2025-07-16 00:56:11 -0300

    cors

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.set("view engine", "ejs");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({ origin: "https://gesstock.local", credentials: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/api", indexRouter);
 
