43199c5445109eb133fc9e84b678d372b78b6b85
Diego <penaomardiego@gmail>
2025-07-18 13:09:17 -0300

    listado de ventas

--- controllers/check/read.js
+++ controllers/check/read.js
@@ -2,18 +2,19 @@ import Check from "../../models/Check.js";
 
 export default async (req, res, next) => {
   try {
-
     let queries = {};
-    if (req.query.descripcion) {
-      queries.descripcion = new RegExp(req.query.descripcion, "i");
+    if (req.query.client_id) {
+      queries.client_id = new RegExp(req.query.client_id, "i");
     }
-    if (req.query.codigoBarras) {
-      queries.codigoBarras = new RegExp(req.query.codigoBarras, "i");
-    }
-    const allCheck = await Check.find(
-      queries,
-      "-__v -createdAt -updatedAt"
-    ).select("").populate("products");
+   
+    const allCheck = await Check.find(queries, "-__v -createdAt -updatedAt")
+      .populate("client_id")
+      .populate({
+        path: "products_id",
+        populate: {
+          path: "id_product", // Cambia 'category' por el campo que deseas poblar dentro de 'products_id'
+        },
+      });
 
     return res.status(200).json({
       success: true,
